066f0c540fc48a29b1aa5fb3b21ef08dcac1ae6b
Arik Hativa <arikhativa@gmail.com>
2023-07-03 09:00:45 +0300

    try with tar

--- script/install_gtest.sh
+++ script/install_gtest.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
+
 sudo apt-get install cmake
-sudo apt-get install libgtest-dev googletest
-cd /usr/src/gtest
+sudo apt-get install googletest
+
+cd /usr/src/googletest
 sudo cmake -Bbuild
 sudo cmake --build build
-sudo cp ./build/lib/libgtest* /usr/lib
-
-
-# gmock
-sudo apt-get -y install google-mock
-
-# export CXX="/usr/bin/c++"
-# cd res
-# tar xf release-1.8.0.tar.gz
-# mv googletest-release-1.8.0 gtest
-# cd gtest
-# sudo cmake -DBUILD_SHARED_LIBS=ON .
-# sudo make
-# sudo cp -a googletest/include/gtest /usr/include
-# sudo cp -a googlemock/gtest/libgtest_main.so googlemock/gtest/libgtest.so /usr/lib/ 
-# sudo cp -a googlemock/gtest/libgtest_main.so googlemock/gtest/libgtest.so ..
-# sudo make install
+sudo cp /usr/src/googletest/build/googlemock/gtest/libgtest*.a /usr/lib
+sudo cp /usr/src/googletest/build/googlemock/libgmock* /usr/lib
