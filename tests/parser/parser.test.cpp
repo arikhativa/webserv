10e76c7ba32406a79fb736bac439b7201ff466f2
Arik Hativa <arikhativa@gmail.com>
2023-07-06 16:49:14 +0300

    parser syntax is done

--- tests/parser/parser.test.cpp
+++ tests/parser/parser.test.cpp
@@ -12,3 +12,51 @@ TEST(parser, FullFile)
 
 	ASSERT_EQ(true, parser::validate(list));
 }
+
+TEST(parser, BadFile0)
+{
+	std::fstream fs("res/tests/parser/parser_error0.conf");
+	std::list<Token> list = lexer::tokenize(fs);
+
+	ASSERT_EQ(false, parser::validate(list));
+}
+
+TEST(parser, BadFile1)
+{
+	std::fstream fs("res/tests/parser/parser_error1.conf");
+	std::list<Token> list = lexer::tokenize(fs);
+
+	ASSERT_EQ(false, parser::validate(list));
+}
+
+TEST(parser, BadFile2)
+{
+	std::fstream fs("res/tests/parser/parser_error2.conf");
+	std::list<Token> list = lexer::tokenize(fs);
+
+	ASSERT_EQ(false, parser::validate(list));
+}
+
+TEST(parser, BadFile3)
+{
+	std::fstream fs("res/tests/parser/parser_error3.conf");
+	std::list<Token> list = lexer::tokenize(fs);
+
+	ASSERT_EQ(false, parser::validate(list));
+}
+
+TEST(parser, BadFile4)
+{
+	std::fstream fs("res/tests/parser/parser_error4.conf");
+	std::list<Token> list = lexer::tokenize(fs);
+
+	ASSERT_EQ(false, parser::validate(list));
+}
+
+TEST(parser, BadFile5)
+{
+	std::fstream fs("res/tests/parser/parser_error5.conf");
+	std::list<Token> list = lexer::tokenize(fs);
+
+	ASSERT_EQ(false, parser::validate(list));
+}
