47c1e09fe6485e55e6232267b835a5188b6f6d85
Arik Hativa <arikhativa@gmail.com>
2023-07-09 17:49:21 +0300

    ErrorPage

--- src/class/HTTPStatusCode/HTTPStatusCode.test.cpp
+++ src/class/HTTPStatusCode/HTTPStatusCode.test.cpp
@@ -17,12 +17,14 @@ TEST(HTTPStatusCode, Accessor)
 	EXPECT_EQ(HTTPStatusCode::Code::NOT_FOUND, obj.get());
 	obj.set(500);
 	EXPECT_EQ(HTTPStatusCode::Code::INTERNAL_SERVER_ERROR, obj.get());
+	obj.set("303");
+	EXPECT_EQ(HTTPStatusCode::Code::SEE_OTHER, obj.get());
 }
 
 TEST(HTTPStatusCode, Canonical)
 {
 	HTTPStatusCode obj1(200);
-	HTTPStatusCode obj2(101);
+	HTTPStatusCode obj2("101");
 	HTTPStatusCode obj3(obj1);
 
 	EXPECT_EQ(obj1.get(), obj3.get());
@@ -35,6 +37,8 @@ TEST(HTTPStatusCode, Exception)
 	EXPECT_THROW(HTTPStatusCode obj(0), HTTPStatusCode::InvalidStatusCodeException);
 	EXPECT_THROW(HTTPStatusCode obj(99), HTTPStatusCode::InvalidStatusCodeException);
 	EXPECT_THROW(HTTPStatusCode obj(506), HTTPStatusCode::InvalidStatusCodeException);
+	EXPECT_THROW(HTTPStatusCode obj("abs"), HTTPStatusCode::InvalidStatusCodeException);
+	EXPECT_THROW(HTTPStatusCode obj("-5"), HTTPStatusCode::InvalidStatusCodeException);
 	HTTPStatusCode obj(200);
 	EXPECT_THROW(obj.set(0), HTTPStatusCode::InvalidStatusCodeException);
 }
