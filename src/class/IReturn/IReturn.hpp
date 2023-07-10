f588ff9561bd5ba461e9d4911fb930d93c3d1d2a
Arik Hativa <arikhativa@gmail.com>
2023-07-10 10:11:53 +0300

    Return

--- src/class/IReturn/IReturn.hpp
+++ src/class/IReturn/IReturn.hpp
@@ -5,16 +5,24 @@
 #include <iostream>
 #include <string>
 
+#include <HTTPStatusCode/HTTPStatusCode.hpp>
+#include <IPath/IPath.hpp>
+
 #ifdef TEST_ON
 #define private public
 #define protected public
 #endif
 
+// TODO maybe HTTPStatusCode should be an interface
 class IReturn
 {
   public:
 	virtual ~IReturn(){};
+	virtual HTTPStatusCode getStatus(void) const = 0;
+	virtual const IPath &getPath(void) const = 0;
 };
+
+std::ostream &operator<<(std::ostream &o, const IReturn &i);
 
 #ifdef TEST_ON
 #undef private
