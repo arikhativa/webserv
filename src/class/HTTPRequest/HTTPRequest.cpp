b2a1d0b1338ed4b351ec8e2d82bea5980fff041c
Alvaro <al7aro@gmail.com>
2023-07-23 13:44:36 +0200

    HTTPRequest

---
+++ src/class/HTTPRequest/HTTPRequest.cpp
@@ -0,0 +1,146 @@
+#include <HTTPRequest/HTTPRequest.hpp>
+
+const std::string HTTPRequest::GET_STRING("GET ");
+const std::string HTTPRequest::POST_STRING("POST ");
+const std::string HTTPRequest::DELETE_STRING("DELETE ");
+
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
+
+const int HTTPRequest::MAX_REQUEST_ATTEMPTS = 10;
+
+HTTPRequest::HTTPRequest(const Server virtualServer, int clientFd)
+	: _attempts(0), _virtualServer(virtualServer), _clientFd(clientFd), _response("\0")
+{
+}
+
+
+const char *HTTPRequest::SendingResponseError::what() const throw()
+{
+	return "Couldn't send response: send() failed";
+}
+
+const char *HTTPRequest::RecievingRequestError::what() const throw()
+{
+	return "Didn't recieve request: recv() failed";
+}
+
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+HTTPRequest::~HTTPRequest()
+{
+}
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+std::ostream &operator<<(std::ostream &o, HTTPRequest const &i)
+{
+	o << "HTTPRequest[" << i.getRawRequest() << "]";
+	return o;
+}
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+HTTPRequest::type HTTPRequest::getRequestType()
+{
+	if (!this->_rawRequest.compare(0, HTTPRequest::GET_STRING.size(), GET_STRING))
+		return (GET);
+	if (!this->_rawRequest.compare(0, HTTPRequest::POST_STRING.size(), POST_STRING))
+		return (POST);
+	if (!this->_rawRequest.compare(0, HTTPRequest::DELETE_STRING.size(), DELETE_STRING))
+		return (DELETE);
+	else
+		return (UNKNOWN);
+}
+
+void HTTPRequest::recvRequest(void)
+{
+	int tmpRecvLen;
+	char tmpRaw[4096];
+
+	tmpRecvLen = recv(this->_clientFd, tmpRaw, sizeof(tmpRaw), MSG_DONTWAIT);
+	if (tmpRecvLen <= -1)
+	{
+		this->_attempts++;
+		throw RecievingRequestError();
+	}
+	this->_rawRequest = std::string(tmpRaw);
+}
+
+void HTTPRequest::sendResponse(void)
+{
+	int sendStatus;
+	sendStatus = send(this->_clientFd, this->_response.c_str(), this->_response.size(), MSG_DONTWAIT);
+	if (sendStatus <= -1)
+	{
+		this->_attempts++;
+		throw SendingResponseError();
+	}
+}
+
+void HTTPRequest::handleRequest(void)
+{
+	switch (getRequestType())
+	{
+		case HTTPRequest::GET:
+			this->_response = HTTPRequestHandler::GET(this->_virtualServer, this->_rawRequest);
+			break;
+		case HTTPRequest::POST:
+			this->_response = HTTPRequestHandler::POST(this->_virtualServer, this->_rawRequest);
+			break;
+		case HTTPRequest::DELETE:
+			this->_response = HTTPRequestHandler::DELETE(this->_virtualServer, this->_rawRequest);
+			break;
+		default: //Uknown request
+			this->_response = HTTPRequestHandler::UNKNOWN(this->_virtualServer, this->_rawRequest);
+			break;
+	}
+}
+
+void HTTPRequest::terminate(void)
+{
+	close(this->_clientFd);
+}
+
+/*
+** --------------------------------- ACCESSOR ---------------------------------
+*/
+
+std::string HTTPRequest::getRawRequest(void) const
+{
+	return this->_rawRequest;
+}
+
+std::string HTTPRequest::getResponse(void) const
+{
+	return this->_response;
+}
+
+int HTTPRequest::getClientFd(void) const
+{
+	return this->_clientFd;
+}
+
+int HTTPRequest::getAttempts(void) const
+{
+	return this->_attempts;
+}
+
+void HTTPRequest::setRawRequest(std::string request)
+{
+	this->_rawRequest = request;
+}
+
+void HTTPRequest::setResponse(std::string response)
+{
+	this->_response = response;
+}
+
+/* ************************************************************************** */
---
+++ src/class/HTTPRequest/HTTPRequest.hpp
@@ -0,0 +1,80 @@
+
+#ifndef H_T_T_P_REQUEST_HPP
+#define H_T_T_P_REQUEST_HPP
+
+#include <HTTPRequestHandler/HTTPRequestHandler.hpp>
+#include <Server/Server.hpp>
+#include <iostream>
+#include <map>
+#include <string>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#ifdef TEST_ON
+#define private public
+#define protected public
+#endif
+
+class HTTPRequest
+{
+  private:
+	enum type
+	{
+		GET,
+		POST,
+		DELETE,
+		UNKNOWN
+	};
+	static const std::string GET_STRING;
+	static const std::string POST_STRING;
+	static const std::string DELETE_STRING;
+
+  public:
+	static const int MAX_REQUEST_ATTEMPTS;
+	explicit HTTPRequest(const Server virtualServer, int client_fd);
+	~HTTPRequest();
+
+	std::string getRawRequest(void) const;
+	std::string getResponse(void) const;
+	int getClientFd(void) const;
+	int getAttempts(void) const;
+
+	void setRawRequest(std::string request);
+	void setResponse(std::string response);
+	void setClientFd(int fd);
+
+	type getRequestType(void);
+	void recvRequest(void);
+	void sendResponse(void);
+	void handleRequest(void);
+	void terminate(void);
+
+	class SendingResponseError : public std::exception
+	{
+	  public:
+		virtual const char *what() const throw();
+	};
+
+	class RecievingRequestError : public std::exception
+	{
+	  public:
+		virtual const char *what() const throw();
+	};
+
+  private:
+	int _attempts;
+	const Server _virtualServer;
+	const int _clientFd;
+
+	std::string _rawRequest;
+	std::string _response;
+};
+
+std::ostream &operator<<(std::ostream &o, HTTPRequest const &i);
+
+#ifdef TEST_ON
+#undef private
+#undef protected
+#endif
+
+#endif /* H_T_T_P_REQUEST_HPP */
