74ddfd2a8649b7e29590f0e27449b62fb2920012
rufo <rufo@rufo.rufo>
2023-07-23 16:47:34 +0200

    Delete request

--- src/code/HTTPRequestHandler/HTTPRequestHandler.cpp
+++ src/code/HTTPRequestHandler/HTTPRequestHandler.cpp
@@ -17,9 +17,34 @@ std::string HTTPRequestHandler::post::POST(Server server, std::string request)
 
 std::string HTTPRequestHandler::delete_::DELETE(Server server, std::string request)
 {
-	(void)request;
 	(void)server;
-	return std::string("You sent a DELETE request:\n");
+	std::string path = "/home/rufo/Desktop/42/web_example/"; //temporal, server.getPath();
+	{
+		std::size_t pos = request.find("\r\n");
+		if (pos != std::string::npos)
+			ILogger::consoleLogDebug("POST request: " + request.substr(0, pos));
+		else
+			ILogger::consoleLogDebug("POST request: " + request);
+	}
+	std::string file = "";
+	std::size_t barra_pos = request.find("/");
+	if (barra_pos != std::string::npos) {
+		std::size_t espacio_pos = request.find(" ", barra_pos);
+		if (espacio_pos != std::string::npos) 
+			 file = request.substr(barra_pos + 1, espacio_pos - barra_pos - 1);
+	}
+	if (!httprequesthandlerDELETE::exists_file(path + file))
+	{
+		ILogger::consoleLogDebug("DELETE file not found(" + path + file + ")");
+		ResponseHeader response(404);
+		return (response.getResponse());
+	}
+	std::remove((path + file).c_str());
+	ILogger::consoleLogDebug("DELETE file deleted(" + path + file + ")");
+	ResponseHeader response(200);
+	response.setBody("<!DOCTYPE html>\n<html>\n<body>\n<h1>File deleted</h1>\n</body>\n</html>");
+	response.setContentType(".html");
+	return (response.getResponse());
 }
 
 std::string HTTPRequestHandler::unknown::UNKNOWN(Server server, std::string request)
--- src/code/HTTPRequestHandler/httprequesthandlerDELETE.hpp
+++ src/code/HTTPRequestHandler/httprequesthandlerDELETE.hpp
@@ -1,10 +1,13 @@
 #ifndef HTTPREQUESTHANDLERDELETE_HPP
 #define HTTPREQUESTHANDLERDELETE_HPP
 
+#include <iostream>
+#include <sys/stat.h>
+#include <cstdio>
 
 namespace httprequesthandlerDELETE
 {
-
+	bool		exists_file(const std::string& path);
 }
 
 #endif
