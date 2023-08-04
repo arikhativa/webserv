353ec0c03a96aac6b42584b80f4c60dc85c8940f
llanahp <raul.lopez@edu.uah.es>
2023-08-04 11:15:15 +0200

    clang format

--- src/code/HTTPRequestHandler/httprequesthandlerDELETE.cpp
+++ src/code/HTTPRequestHandler/httprequesthandlerDELETE.cpp
@@ -1,25 +1,25 @@
 #include <HTTPRequestHandler/httprequesthandlerDELETE.hpp>
 
-bool	httprequesthandlerDELETE::fileExists(const std::string& path)
+bool httprequesthandlerDELETE::fileExists(const std::string &path)
 {
 	struct stat statbuf;
 	return (stat(path.c_str(), &statbuf) == 0);
 }
 
-bool	httprequesthandlerDELETE::isDirectory(const std::string& path)
+bool httprequesthandlerDELETE::isDirectory(const std::string &path)
 {
 	struct stat statbuf;
 	stat(path.c_str(), &statbuf);
 	return (S_ISDIR(statbuf.st_mode));
 }
 
-void	httprequesthandlerDELETE::deleteFile(const std::string& path)
+void httprequesthandlerDELETE::deleteFile(const std::string &path)
 {
 	if (remove(path.c_str()) != 0)
 		throw httprequesthandlerDELETE::deleteException();
 }
 
-const char* httprequesthandlerDELETE::deleteException::what() const throw()
+const char *httprequesthandlerDELETE::deleteException::what() const throw()
 {
 	return ("Failed to delete file");
 }
