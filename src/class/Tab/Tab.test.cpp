370db2ecabb80b754a577aa4811b8172889d0ef9
llanahp <raul.lopez@edu.uah.es>
2023-08-10 19:35:16 +0200

    More test Tab class

--- src/class/Tab/Tab.test.cpp
+++ src/class/Tab/Tab.test.cpp
@@ -8,7 +8,38 @@ TEST(Tab, CreateDestroy)
 	delete obj;
 }
 
-TEST(Tab, a)
+TEST(Tab, get)
+{
+	Tab obj1;
+
+	obj1.add("Content-Length=42");
+	obj1.add("Content-Type=text/html");
+	obj1.add("Host=localhost:8080");
+	obj1.add("Accept-Language=en-US,en;q=0.5");
+	obj1.add("Accept-Encoding=gzip, deflate");
+
+	EXPECT_EQ(obj1.get(0), "Content-Length=42");
+	EXPECT_EQ(obj1.get(1), "Content-Type=text/html");
+	EXPECT_EQ(obj1.get(2), "Host=localhost:8080");
+	EXPECT_EQ(obj1.get(3), "Accept-Language=en-US,en;q=0.5");
+	EXPECT_EQ(obj1.get(4), "Accept-Encoding=gzip, deflate");
+}
+
+TEST(Tab, freeTab)
+{
+	Tab obj1;
+
+	obj1.add("Content-Length=42");
+	obj1.add("Content-Type=text/html");
+	obj1.add("Host=localhost:8080");
+	obj1.add("Accept-Language=en-US,en;q=0.5");
+	obj1.add("Accept-Encoding=gzip, deflate");
+
+	char **res = obj1.toCTable();
+	obj1.freeTab(res);
+}
+
+TEST(Tab, toCTableContent)
 {
 	Tab obj1;
 
