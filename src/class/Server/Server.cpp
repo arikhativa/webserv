50521ffe1019a05893c1d95b11883d15a5e51e5a
Alvaro Lopez <al7aro@gmail.com>
2023-09-02 01:07:00 +0200

    lint

--- src/class/Server/Server.cpp
+++ src/class/Server/Server.cpp
@@ -76,7 +76,7 @@ void Server::listenSockets()
 ** --------------------------------- ACCESSOR ---------------------------------
 */
 
-//Hacer que estot devuelva los sockets y no los file descriptors
+// Hacer que estot devuelva los sockets y no los file descriptors
 const std::vector<int> Server::getSockets(void) const
 {
 	std::vector<int> fds;
