622f023a029071ef6f5e9a0804e94cecb828847b
llanahp <raul.lopez@edu.uah.es>
2023-09-11 12:19:20 +0200

    merge

--- src/code/HTTPRequestHandlerGET/HTTPRequestHandlerGET.cpp
+++ src/code/HTTPRequestHandlerGET/HTTPRequestHandlerGET.cpp
@@ -5,8 +5,7 @@ bool httprequesthandlerGET::isDirectoryListing(const Path &path, const HTTPCall 
 	return (FileManager::isDirectory(path.get()) && matcher::isAutoIndexOnToRequest(request));
 }
 
-std::string httprequesthandlerGET::getFileContent(const std::string &path, ResponseHeader &response,
-												  HTTPRequest &request)
+std::string httprequesthandlerGET::getFileContent(const std::string &path, ResponseHeader &response, HTTPCall &request)
 {
 	// if (isCgi(void))
 	//{
@@ -17,8 +16,8 @@ std::string httprequesthandlerGET::getFileContent(const std::string &path, Respo
 		throw std::runtime_error("Error: socket is null");
 	Port port = request.getSocket()->getPort();
 
-	CgiManager obj1(request.getBasicRequest(), pathCGI, serverName, port.get() + "");
-	return (obj1.executeCgiManager(request.getPathServerDirectory()));
+	CgiManager cgi_obj(request.getBasicRequest(), pathCGI, serverName, port.get() + "");
+	return (cgi_obj.executeCgiManager(matcher::rootToRequest(request)));
 	//}
 	if (path.find(".") != std::string::npos)
 		response.setContentType(path.substr(path.find_last_of(".")));
--- src/code/HTTPRequestHandlerGET/HTTPRequestHandlerGET.hpp
+++ src/code/HTTPRequestHandlerGET/HTTPRequestHandlerGET.hpp
@@ -14,13 +14,14 @@
 #include <HTTPRequestHandler/HTTPRequestHandler.hpp>
 #include <ResponseHeader/ResponseHeader.hpp>
 #include <Server/Server.hpp>
+#include <matcher/matcher.hpp>
 
 class HTTPCall;
 
 namespace httprequesthandlerGET
 {
 	bool isDirectoryListing(const Path &path, const HTTPCall &request);
-	std::string getFileContent(const std::string &path, ResponseHeader &response);
+	std::string getFileContent(const std::string &path, ResponseHeader &response, HTTPCall &request);
 	std::string getDirecoryContent(const Path &path, const Path &name);
 } // namespace httprequesthandlerGET
 
