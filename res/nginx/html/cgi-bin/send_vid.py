fd17ddd3e7d7326b96ebcedae256d49c23509cee
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2023-09-30 16:51:11 +0200

    done

---
+++ res/nginx/html/cgi-bin/send_vid.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+file_path = 'res/nginx/html/cgi-bin/tmp.mp4'
+
+try:
+    with open(file_path, 'rb') as file:
+        content = file.read()
+        sys.stdout.buffer.write(content)
+except FileNotFoundError as e:
+    print(f"The file was not found: {e}")
+except PermissionError as e:
+    print(f"You don't have permission to access this file: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
