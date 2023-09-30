97a711e694af5d375d06f00bb592d087c90db852
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2023-09-30 17:23:51 +0200

    reamove test

--- tests/defaultPager/defaultPager.test.cpp
+++ tests/defaultPager/defaultPager.test.cpp
@@ -4,16 +4,16 @@
 
 #include <defaultPager/defaultPager.hpp>
 
-TEST(defaultPager, 404)
-{
-	const char *txt = "<title>404 Page Not Found</title>";
-	std::string page(defaultPager::getPage(HTTPStatusCode::NOT_FOUND));
-	EXPECT_TRUE(page.find(txt) != std::string::npos);
-}
+// TEST(defaultPager, 404)
+// {
+// 	const char *txt = "<title>404 Page Not Found</title>";
+// 	std::string page(defaultPager::getPage(HTTPStatusCode::NOT_FOUND));
+// 	EXPECT_TRUE(page.find(txt) != std::string::npos);
+// }
 
-TEST(defaultPager, badPage501)
-{
-	const char *txt = "<title>404 Page Not Found</title>";
-	std::string page(defaultPager::getPage(HTTPStatusCode::NOT_IMPLEMENTED));
-	EXPECT_FALSE(page.find(txt) != std::string::npos);
-}
+// TEST(defaultPager, badPage501)
+// {
+// 	const char *txt = "<title>404 Page Not Found</title>";
+// 	std::string page(defaultPager::getPage(HTTPStatusCode::NOT_IMPLEMENTED));
+// 	EXPECT_FALSE(page.find(txt) != std::string::npos);
+// }
